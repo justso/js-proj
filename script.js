0c886b7cb886b55c4868110e45edbc26aeea8742
dvdrtrgn <dvdrtrgn@gmail.com>
2013-04-14 16:02:10 -0500

    cleanup

--- script.js
+++ script.js
@@ -1,34 +1,78 @@
+/*jslint es5:true, forin:true, white:false */
+/*globals $, jQuery, console */
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 // ASYNC DEMOS
 
-function timeout(f, n){ setTimeout(f, n||1) }
-function linger(n){
-    var soon = $.now() + 3000;
-    while($.now() < soon) void(0);
-    shout('done');
+function clog() {
+    console.log.apply(console, arguments);
 }
-function shout(s){ console.log(s) }
 
-function Demo1 () {
-    shout('a');
-    timeout(function(){ shout('c') }, 500);
-    timeout(function(){ shout('d') }, 499);
-    timeout(linger, 0);
-    shout('b');
+function queue(f, n) {
+    setTimeout(f, n || 1);
+}
+
+function linger(n) {
+    var soon = $.now() + 3000;
+    while ($.now() < soon) {
+        $.noop();
+    }
+    clog('done');
+}
+
+
+function Demo1() {
+    clog('a');
+
+    queue(function () {
+        clog('c');
+    }, 500);
+
+    queue(function () {
+        clog('d');
+    }, 499);
+
+    queue(linger, 0);
+    clog('b');
     // a
     // b
     // undefined
-    // done ... linger takes a few secondsfinishes before c and d!
+    // done ... linger takes a few seconds / finishes before c and d!
     // d
     // c
 }
 
-function Demo2 () {
-    var fromAddress, toAddress, GMaps = {};
+function Demo2() {
+    var fromAddress = 'A',
+        toAddress = 'B',
+        GMaps = {
+        geocode: function (obj) {
+            queue(function () {
+                clog('GMaps.geocode', obj);
+                obj.callback({
+                    lat: function () {
+                        return obj.address + 'x';
+                    },
+                    lng: function () {
+                        return obj.address + 'y';
+                    },
+                });
+            }, 999);
+        },
 
-    GMaps.geocode = function () {
-        console.log(arguments);
-        return arguments;
-    }
+        getRoutes: function (obj) {
+            queue(function () {
+                clog('GMaps.getRoutes', obj);
+                obj.callback(arguments);
+            }, 999);
+        },
+        drawDirections: function (route) {
+            // do something with route
+            queue(function () {
+                clog('GMaps.drawDirections', arguments);
+            }, 999);
+        },
+    };
 
     function geocode(address) {
         var dfd = new $.Deferred();
@@ -36,34 +80,30 @@ function Demo2 () {
             address: address,
             callback: function (response, status) {
                 return dfd.resolve(response);
-            }
+            },
         });
         return dfd.promise();
-    };
+    }
 
     function getRoute(fromLatLng, toLatLng) {
         var dfd = new $.Deferred();
-        map.getRoutes({
-            origin: [fromLatLng.lat(), fromLatLng.lng()],
-            destination: [toLatLng.lat(), toLatLng.lng()],
+        GMaps.getRoutes({
+            origin: fromLatLng.lat() + '+' + fromLatLng.lng(),
+            destination: toLatLng.lat() + '+' + toLatLng.lng(),
             travelMode: "driving",
             unitSystem: "imperial",
             callback: function (e) {
                 return dfd.resolve(e);
-            }
+            },
         });
         return dfd.promise();
-    };
-
-    function doSomethingCoolWithDirections(route) {
-        // do something with route
-        console.log(arguments);
-    };
+    }
 
     $.when(geocode(fromAddress), geocode(toAddress)).
     then(function (fromLatLng, toLatLng) {
-        getRoute(fromLatLng, toLatLng).then(doSomethingCoolWithDirections);
+        getRoute(fromLatLng, toLatLng).then(GMaps.drawDirections);
     });
 
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
